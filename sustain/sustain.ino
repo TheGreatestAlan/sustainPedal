b5335ad15996dc10f4d9820cae9945ccb9466e5b
thegreatestalan <alan.hoang.nguyen@gmail.com>
2022-11-06 18:32:15 -0700

    updating to working spec

--- sustain/sustain.ino
+++ sustain/sustain.ino
@@ -1,26 +1,40 @@
 #include <MIDI.h>
 
-MIDI_CREATE_DEFAULT_INSTANCE();
+struct MySettings : public midi::DefaultSettings
+{
+    static const long BaudRate = 115200;
+};
+
+MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial, MIDI, MySettings);
 
 // the setup routine runs once when you press reset:
 void setup() {
-  MIDI.begin();
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+  
 }
 
 // the loop routine runs over and over again forever:
 bool sustain = false;
 void loop() {
+
   // read the input on analog pin 0:
   int sensorValue = analogRead(A0);
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (5.0 / 1023.0);
+  float voltage= sensorValue * (5.0 / 1023.0);
+  //Serial.println(voltage);
+
   // print out the value you read:
-  if (voltage >= 3.1 && sustain == false) {
-    MIDI.sendNoteOn(66, 127, 1);
+  if (voltage >= 3.31 && sustain == false) {
+    MIDI.sendControlChange(64, 127, 1);
     sustain = true;
-  }
-  if (voltage != 3.1 && sustain == true) {
-    MIDI.sendNoteOn(66, 0, 1);
+    //Serial.println(voltage);
+
+    //Serial.println("Pedal down");
+  }else if (voltage < 3.2 && sustain == true) {
+    MIDI.sendControlChange(64, 1, 1);
     sustain = false;
+    //Serial.println(voltage);
+
+    //Serial.println("Pedal up");
   }
 }
